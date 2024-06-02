6afdfbe4d51263853716116ecd0a8961fdbe70e1
Leoncio97 <jonathanleoncio97@gmail.com>
2024-06-02 00:09:31 -0300

    content created & finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "marvel anuncia um novo filme do homem de ferro, confira",
+        "photoCover": "https://www.postergully.com/cdn/shop/products/PG57_Copy_12721807-4ae9-45d8-b988-4c025d38897c.jpg?v=1578633816"
+    },
+    {
+        "id":"2",
+        "title": "Nova série anunciada no Disney +",
+        "description": "blabla bla bla bblaaa",
+        "photoCover": "https://s2-techtudo.glbimg.com/GTAsxXICB1qrOrEDPg5XsK7Tcrs=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/3/2/xYDHyVSn6KaTnDRNepoQ/63.png"
+    }
+]
