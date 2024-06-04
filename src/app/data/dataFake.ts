94c8390b5ac8398f925ab791806fc468e4cb2247
Leoncio97 <jonathanleoncio97@gmail.com>
2024-06-04 00:25:23 -0300

    Merge branch 'main' of https://github.com/Leonciosouza/angular-blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -8,7 +8,21 @@ export const dataFake = [
     {
         "id":"2",
         "title": "Nova série anunciada no Disney +",
-        "description": "blabla bla bla bblaaa",
+        "description": "O Disney agora quer produzir novas versões para seus filmes, assim como os da Marvel.",
         "photoCover": "https://s2-techtudo.glbimg.com/GTAsxXICB1qrOrEDPg5XsK7Tcrs=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/3/2/xYDHyVSn6KaTnDRNepoQ/63.png"
+    },
+
+    {
+        "id":"3",
+        "title": "Novo filme do Pantera Negra anunciado em breve, nos cinemas e canais de streams",
+        "description": "Um herói dotado de incrível velocidade, força e agilidade. Compondo parte dos personagens do grupo 'Os vingadores', é um marco cultural para todo o universo geek.",
+        "photoCover": "https://akamai.sscdn.co/uploadfile/letras/playlists/2/d/6/c/2d6c659db1bc40358ecb98a06300be1a.jpg"
+    },
+
+    {
+        "id":"4",
+        "title": "Novo filme dos X-men está em fase de Pós produção, afirma diretor",
+        "description": "É uma das maiores franquias de filmes de heróis já criadas, mas até mesmo um grande fã pode achar partes do enredo um pouco confusas.\nPois a franquia dos X-men é uma coleção de 13 filmes que adotam uma abordagem não linear, semelhante à de Star Wars,\ne começam mais tarde nas histórias de muitos dos personagens antes de voltar às suas origens.",
+        "photoCover": "https://images.bauerhosting.com/legacy/empire-tmdb/films/127585/images/ai9UKd8KownQKGIh1m5p3DuEeMm.jpg?ar=16%3A9&fit=crop&crop=top&auto=format&w=1440&q=80"
     }
 ]
